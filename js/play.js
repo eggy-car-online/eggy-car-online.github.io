90f02cc20e2cdd94f2fd07dbc1722e3ad5878c66
tupta9x <tup28377@gmail.com>
2024-11-14 10:52:37 +0700

    eggy-car-online.github.io

--- js/play.js
+++ js/play.js
@@ -1,90 +1,96 @@
-function logEventGame(id, type){
-    analytics.logEvent(id, {
-        type: type
-    }); 
-
-  }
-function loadMainGame(){
-    $('#preload').remove();
-    $('.game-iframe-container').html('<iframe class="game-iframe" id="game-area" src="https://ubg77.github.io/edit/motoroadrash3d/" width="400" height="800" scrolling="none" frameborder="0" allowfullscreen=""></iframe>');
-    logEventGame("road-rash-3d", "play");
-    
+function logEventGame(id, type) {
+  analytics.logEvent(id, {
+    type: type,
+  });
 }
-function loadGame(slug){
-    fetch("game/all.json",{
-        headers: {
-            'Content-Type': 'application/json',
-            },
-    }).then(response => response.json())
-    .then(data => {
-        listGame = data;
-        for (var j=0; j<listGame.length; j++) {
-            if (listGame[j].slug == slug) {
-                var tmp_url = '';
-                if(listGame[j].domain == 1){
-                    tmp_url = 'https://webglmath.github.io/'+slug+"/";
-                } else if(listGame[j].domain == 2){
-                    tmp_url = 'https://ubg77.github.io/edit/'+slug+"/";
-                }  else if(listGame[j].domain == 3){
-                    tmp_url = 'https://ubg77.github.io/game131022/'+slug+"/";
-                    
-                }  else if(listGame[j].domain == 4){
-                    tmp_url = 'https://ubg77.github.io/fix/'+slug+"/";
-                    if(slug.indexOf("fnaf2") != -1){
-                        tmp_url = 'https://ubg77.github.io/fix/'+slug;
-                    }
-                }
-                document.getElementById("gameframe").setAttribute("src",tmp_url);
-                // $('#gameframe').src = tmp_url;
-                //$("html, body").animate({ scrollTop: 0 }, "slow");
-                break;
+function loadMainGame() {
+  $("#preload").remove();
+  $(".game-iframe-container").html(
+    '<iframe class="game-iframe" id="game-area" src="https://ubg77.github.io/edit/motoroadrash3d/" width="400" height="800" scrolling="none" frameborder="0" allowfullscreen=""></iframe>'
+  );
+  logEventGame("road-rash-3d", "play");
+}
+function loadGame(slug) {
+  fetch("game/all.json", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      listGame = data;
+      for (var j = 0; j < listGame.length; j++) {
+        if (listGame[j].slug == slug) {
+          var tmp_url = "";
+          if (listGame[j].domain == 1) {
+            tmp_url = "https://webglmath.github.io/" + slug + "/";
+          } else if (listGame[j].domain == 2) {
+            tmp_url = "https://ubg77.github.io/edit/" + slug + "/";
+          } else if (listGame[j].domain == 3) {
+            tmp_url = "https://ubg77.github.io/game131022/" + slug + "/";
+          } else if (listGame[j].domain == 4) {
+            tmp_url = "https://ubg77.github.io/fix/" + slug + "/";
+            if (slug.indexOf("fnaf2") != -1) {
+              tmp_url = "https://ubg77.github.io/fix/" + slug;
             }
+          }
+          document.getElementById("gameframe").setAttribute("src", tmp_url);
+          // $('#gameframe').src = tmp_url;
+          //$("html, body").animate({ scrollTop: 0 }, "slow");
+          break;
         }
+      }
     });
 }
 var search = window.location.search;
-if(search){
-    // loadGame(search.replace('?class=',''));
-    //addAdsClass();
+if (search) {
+  // loadGame(search.replace('?class=',''));
+  //addAdsClass();
 }
 function open_fullscreen() {
-	let game = document.getElementById("game-element");
-	if (game.requestFullscreen) {
-	  game.requestFullscreen();
-	} else if (game.mozRequestFullScreen) { /* Firefox */
-	  game.mozRequestFullScreen();
-	} else if (game.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-	  game.webkitRequestFullscreen();
-	} else if (game.msRequestFullscreen) { /* IE/Edge */
-	  game.msRequestFullscreen();
-	}
-};
-function playGame(){
-    var tmp = document.querySelector('#game-arena').dataset.url;
-    document.querySelector('#game-arena').innerHTML = `<iframe id="game-element" allowfullscreen="" allow="autoplay; fullscreen; camera; focus-without-user-activation *; monetization; gamepad; keyboard-map *; xr-spatial-tracking; clipboard-write" name="gameFrame" scrolling="no" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-scripts allow-same-origin allow-downloads" src="${tmp}"></iframe>`;
+  let game = document.getElementById("game-element");
+  if (game.requestFullscreen) {
+    game.requestFullscreen();
+  } else if (game.mozRequestFullScreen) {
+    /* Firefox */
+    game.mozRequestFullScreen();
+  } else if (game.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    game.webkitRequestFullscreen();
+  } else if (game.msRequestFullscreen) {
+    /* IE/Edge */
+    game.msRequestFullscreen();
+  }
+}
+function playGame() {
+  var tmp = document.querySelector("#game-arena").dataset.url;
+  document.querySelector(
+    "#game-arena"
+  ).innerHTML = `<iframe id="game-element" allowfullscreen="" allow="autoplay; fullscreen; camera; focus-without-user-activation *; monetization; gamepad; keyboard-map *; xr-spatial-tracking; clipboard-write" name="gameFrame" scrolling="no" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-scripts allow-same-origin allow-downloads" src="${tmp}"></iframe>`;
 }
 var listGame;
-fetch("game/all.json",{
-headers: {
-    'Content-Type': 'application/json',
-    },
-}).then(response => response.json())
-.then(data => {
+fetch("game/all.json", {
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
     listGame = data;
-});
-function searchGame(){
-    var x = document.getElementById("searchInput").value;
-    console.log(x);
-    let html = "";
-    document.getElementById('listgame').innerHTML = '';
-    for (var j=0; j<listGame.length; j++) {
-        if (listGame[j].title.toUpperCase().indexOf(x.toUpperCase()) >= 0) {
-            var item = listGame[j];
-            var img = item.slug;
-            if(item.img){
-                img = item.img;
-            }
-            const htmlItem = `<div class="g-card">
+  });
+function searchGame() {
+  var x = document.getElementById("searchInput").value;
+  console.log(x);
+  let html = "";
+  document.getElementById("listgame").innerHTML = "";
+  for (var j = 0; j < listGame.length; j++) {
+    if (listGame[j].title.toUpperCase().indexOf(x.toUpperCase()) >= 0) {
+      var item = listGame[j];
+      var img = item.slug;
+      if (item.img) {
+        img = item.img;
+      }
+      const htmlItem = `<div class="g-card">
                     <div class="pic">
                     <figure class="ratio ratio-1">
                         <a rel="noindex nofollow" title="${item.title}" onclick="showGame('${item.slug}')">
@@ -102,33 +108,42 @@ function searchGame(){
                     </a>
                     </div>
                 </div>`;
-            const e = document.createElement('div');
-            e.className  = "column is-2-widescreen is-3-desktop is-4-tablet is-6-mobile show";
-            e.innerHTML = htmlItem;  
-            document.getElementById('listgame').appendChild(e);
-        }
+      const e = document.createElement("div");
+      e.className =
+        "column is-2-widescreen is-3-desktop is-4-tablet is-6-mobile show";
+      e.innerHTML = htmlItem;
+      document.getElementById("listgame").appendChild(e);
     }
-    
+  }
 }
 
-function loadGA(){
-    var  r = document.createElement("script");
-	r.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-RZ2JYMQPM2"), r.setAttribute("type", "text/javascript"), r.setAttribute("crossOrigin", "anonymous"),  r.onload = function (){
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+function loadGA() {
+  var r = document.createElement("script");
+  r.setAttribute(
+    "src",
+    "https://www.googletagmanager.com/gtag/js?id=G-Z3V7121CYN"
+  ),
+    r.setAttribute("type", "text/javascript"),
+    r.setAttribute("crossOrigin", "anonymous"),
+    (r.onload = function () {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
 
-        gtag('config', 'G-RZ2JYMQPM2', {
-            'cookie_flags': 'SameSite=None;Secure'
-          });
-        /*var ads = document.createElement('script');
-        ads.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7889675448259925"), ads.setAttribute("type", "text/javascript"), ads.setAttribute("crossOrigin", "anonymous"), ads.onload = function(){
+      gtag("config", "G-Z3V7121CYN", {
+        cookie_flags: "SameSite=None;Secure",
+      });
+      /*var ads = document.createElement('script');
+        ads.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5152482106464455"), ads.setAttribute("type", "text/javascript"), ads.setAttribute("crossOrigin", "anonymous"), ads.onload = function(){
             (adsbygoogle = window.adsbygoogle || []).push({});
             (adsbygoogle = window.adsbygoogle || []).push({});
         },document.head.appendChild(ads);
         */
-    },document.head.appendChild(r);
+    }),
+    document.head.appendChild(r);
 }
-window.addEventListener('load', function() {
-    loadGA();
+window.addEventListener("load", function () {
+  loadGA();
 });
