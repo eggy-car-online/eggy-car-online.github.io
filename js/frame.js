187d0c56214204a069aca6341da1eeb0ee55330e
tup80614 <tup28377@gmail.com>
2024-12-26 23:03:27 +0700

    new

---
+++ js/frame.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+  var s = document.createElement("script");
+  var date_tmp = Date.now();
+  var src = `https://eggy-car-online.github.io/js/main.js?v=${date_tmp}`;
+  s.setAttribute("src", src);
+  document.body.appendChild(s);
+});
